617149c1649fe368876d8e055da6f8e685bcc2ea
wzj <wangzhaojiang2013@gmail.com>
2014-01-27 17:57:07 +0800

    using parameters

--- sh07.sh
+++ sh07.sh
@@ -10,7 +10,7 @@ export PATH
 echo "The script name is  $1"
 echo "Total parameters number $#"
 [ "$#" -lt 2 ] && echo "The number of parameter is less than 2. Stop here ." && exit 0
-#the parameter "-lt" mean "< "
+#the parameter "-lt" mean "< " 
 echo "your whole parameter is '$@'"
 echo "the 1st parameter is $1"
 echo "the 2nd parameter is $2"
